b742922518b7fc407d0b598731adb8efeca23e51
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-02-18 14:22:11 -0800

    Feat: Added auth using clerk. Sign up and sign in routes added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (<div>
     <h1>PlayAI Book Reader</h1>
+    <SignedIn>
+      <SignOutButton>
+        <Button>Sign Out</Button>
+      </SignOutButton>
+    </SignedIn>
+    <SignedOut>
+      <SignInButton>
+        <Button>Sign In</Button>
+      </SignInButton>
+    </SignedOut>
   </div>);
 }
