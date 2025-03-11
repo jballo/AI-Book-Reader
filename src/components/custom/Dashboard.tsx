7afefd6373c87e428d47114edafdf5ce32fc17d4
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-03-11 14:52:39 -0700

    Feat: Added relevant dashboard jsx into 'Header' component.

--- src/components/custom/Dashboard.tsx
+++ src/components/custom/Dashboard.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
-import { OctagonX } from "lucide-react";
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "../ui/alert";
 import PdfView from "./PdfView";
 import PdfListView from "./PdfListView";
 import FileUploader from "./FileUploader";
 import InformationalBanner from "./InformationalBanner";
+import Header from "./Header";
 
 
 interface BasePDF {
@@ -158,34 +153,7 @@ export default function Dashboard({ uploadPdf, uploadPdfMetadata, listPdfs, conv
         <InformationalBanner />
         
         <div className="flex flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            <header className="flex justify-between items-center mb-12">
-                {popUpActive && (
-                    <Alert className="w-80 absolute top-24 left-1/2 transform -translate-x-1/2 z-50 bg-[#C1FF7A] border-black">
-                        <AlertTitle className="flex flex-row gap-2 text-xl"> <OctagonX /> Alert!</AlertTitle>
-                        <AlertDescription className="flex flex-row justify-between text-lg">
-                            Sign In To Use Features
-                            <SignInButton>
-                                <Button>Sign In</Button>
-                            </SignInButton>
-                        </AlertDescription>
-                    </Alert>
-                )}
-                <h1 onClick={() => {
-                    setPdfView(false);
-                }} className="text-5xl font-bold tracking-tight">PlayAI Book Reader</h1>
-                <div className="flex items-center space-x-2">
-                    <SignedIn>
-                        <SignOutButton>
-                            <Button>Sign Out</Button>
-                        </SignOutButton>
-                    </SignedIn>
-                    <SignedOut>
-                        <SignInButton>
-                            <Button>Sign In</Button>
-                        </SignInButton>
-                    </SignedOut>
-                </div>
-            </header>
+            <Header popUpActive={popUpActive} setPdfView={setPdfView} />
             
             {(!pdfFile || !pdfView) ? (
                 <div className="space-y-8">
